66adc50a4d8ea41cfe3a0cffecbff670a029764d
Ali Boudjemai <alboudje@z3r8p5.42lyon.fr>
2023-01-10 09:54:17 +0100

    Added commands and env_var functions prototype

--- src/minishell.h
+++ src/minishell.h
@@ -6,7 +6,52 @@
 /*   By: alboudje <alboudje@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 13:37:04 by alboudje          #+#    #+#             */
-/*   Updated: 2023/01/09 13:37:05 by alboudje         ###   ########.fr       */
+/*   Updated: 2023/01/10 09:53:18 by alboudje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+typedef struct s_env_var
+{
+	char				*name;
+	char				*content;
+	typedef s_env_var	*next;
+}	t_env_var;
+
+typedef struct s_command
+{
+	int			fd_in;
+	int			fd_out;
+	int			fd_err;
+	char		*cmd;
+	char		*args;
+	t_env_var	*vars;
+}	t_command;
+
+typedef struct s_commands
+{
+	t_command	*cmd;
+	t_command	*next;
+}	t_commands;
+
+/*
+	command init and destruction
+*/
+t_command	*init_command(char *cmd, char *args, t_env_var *vars);
+void		set_fd(t_command **cmd, int fd_in, int fd_out, int fd_err);
+void		destroy_command(t_command *cmd);
+
+void		add_command(t_commands **cmds_list, t_command *cmd);
+void		rm_command(t_commands **cmds_list);
+
+/*
+	env variables functions
+*/
+t_env_var	*new_var(char *name, char *content);
+void		free_var(t_env_var *var);
+void		add_var(t_env_var *vars, t_env_var *new_var);
+void		destroy_vars(t_env_var **vars);
+
+#endif
