d0bd2a880d83217a1f76833530074cc797a4f3f3
Ali Boudjemai <alboudje@z4r6p7.42lyon.fr>
2023-01-27 10:58:41 +0100

    Fixed siquit

--- src/minisignals.c
+++ src/minisignals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minisignals.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tibernot <tibernot@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alboudje <alboudje@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 11:26:31 by alboudje          #+#    #+#             */
-/*   Updated: 2023/01/26 14:43:10 by tibernot         ###   ########.fr       */
+/*   Updated: 2023/01/27 10:47:23 by alboudje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,12 @@ void	sigint(int sig)
 void	sigquit_process(int sig)
 {
 	(void)sig;
-	ft_putstr_fd("Quit: 3\n", 2);
+	ft_putstr_fd("Quit: 3\n", 1);
 	exit(131);
 }
 
 void	sigquit(int sig)
 {
 	(void)sig;
-	//ft_putstr_fd("\n", 2);
+	rl_redisplay();
 }
